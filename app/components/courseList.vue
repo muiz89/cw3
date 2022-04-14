536927f6352e207c37a3c3776d05020b2de906ca
Muiz <92529806+muiz89@users.noreply.github.com>
2022-04-14 17:41:41 +0400

    Create courseList.vue

---
+++ app/components/courseList.vue
@@ -0,0 +1,26 @@
+<template>
+    <ListView for="course in courses" @itemTap="onItemTap" style="color:black;height:100%">
+        <v-template>
+            <GridLayout columns="70, auto, *" rows="90, auto, *" width="400"
+                height="200" backgroundColor="blanchedalmond">
+                <Label :text="`$${course.price}`" row="2" col="1" />
+                <Label :text="course.location" row="2" col="2" rowSpan="2" />
+                <Label :text="course.subject" row="1" col="1" />
+            </GridLayout>
+        </v-template>
+    </ListView>
+</template>
+
+<script>
+    export default {
+        props: ["courses"],
+        data() {
+            return {};
+        },
+        methods: {
+            onItemTap(event) {
+                this.$emit("addCourse", event.item);
+            }
+        }
+    };
+</script>
